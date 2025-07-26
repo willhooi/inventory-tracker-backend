e388c41b4148ca292378f36ab437e820f37621e9
Will Hooi <william.hooi@gmail.com>
2025-07-26 12:41:50 +0800

    lets do this

---
+++ src/main/java/com/simple_inventory_tracker/project/ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.simple_inventory_tracker.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simple_inventory_tracker/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple_inventory_tracker.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
